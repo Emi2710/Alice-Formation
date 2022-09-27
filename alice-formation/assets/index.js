aa70221522a0542c4f067695661f0d5207fdb689
Emi2710 <deadourfreedom@gmail.com>
2022-09-27 19:40:55 +0200

    extracted my files

--- alice-formation/assets/index.js
+++
@@ -1,5 +0,0 @@
-import alice from './ALICE.svg';
-import dropdown from './dropdown.svg'
-import close from './close.svg'
-
-export { alice, dropdown, close }
--- alice-formation/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Article } from './Article';
-export { default as HeroBanner } from './HeroBanner';
-export { default as Navbar } from './Navbar';
-export { default as EmailForm } from './EmailForm';
-export { default as Footer } from './Footer';
