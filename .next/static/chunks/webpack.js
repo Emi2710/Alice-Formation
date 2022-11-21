de835e3d157c7c3d720f849f9848196edc7f7eac
Emi2710 <deadourfreedom@gmail.com>
2022-11-21 22:15:05 +0100

    update slug generate

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0c3daf791dc71ee3"; }
+/******/ 		__webpack_require__.h = function() { return "c93ec8daf2ad71bc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
