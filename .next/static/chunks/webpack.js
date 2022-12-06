42c399bb70728b571c1cc4e29e683ef587f76256
Emi2710 <deadourfreedom@gmail.com>
2022-12-06 14:53:12 +0100

    adding a live chatbot

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "19eaaec259c5f5d8"; }
+/******/ 		__webpack_require__.h = function() { return "9c6cb6a4a4bbcb3f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import Head from 'next/head'
+import Head from 'next/head';
+import Script from 'next/script';
 import '../styles/globals.css';
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-      
-    </Head>
+    <Script type="text/javascript" id="hs-script-loader" async defer src="//js-eu1.hs-scripts.com/26686180.js"></Script>
     <Component {...pageProps} />
     </>
     
