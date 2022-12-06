262886a8f1ee75845f40dd3b866989a0d3cb3990
Emi2710 <deadourfreedom@gmail.com>
2022-12-06 16:31:11 +0100

    fixing minor design issue

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c69e66d46e4c8557"; }
+/******/ 		__webpack_require__.h = function() { return "8f82ba36fad6291b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
