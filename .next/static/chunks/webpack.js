8689961622133c1a3a83a91fc7b6e25da08ba470
Emi2710 <deadourfreedom@gmail.com>
2022-12-07 01:36:54 +0100

    fixing a margin error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e6798a1dad9bc5b9"; }
+/******/ 		__webpack_require__.h = function() { return "559765a538a38494"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
